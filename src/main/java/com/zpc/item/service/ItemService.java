588f5c8bed7f6e84f9b7fc1392434bb91c8edd51
lcm1 <1272531861@qq.com>
2020-04-30 16:30:48 +0800

    Merge branch 'first'
    # Conflicts:
    #	src/main/java/com/zpc/item/service/ItemService.java

--- src/main/java/com/zpc/item/service/ItemService.java
+++ src/main/java/com/zpc/item/service/ItemService.java
@@ -32,7 +32,6 @@ public class ItemService {
      */
     public Item queryItemById(Long id) {
         System.out.println("当前端口为：8081");
-        System.out.println("testtesttest");
         System.out.println("111111111111");
         System.out.println("22222222222");
         System.out.println("111111111111");
